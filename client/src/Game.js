207dcd6ffdb9fae1b6b6f2d7a8bbc29a4366bcda
Kevin T <kdt0116@tamu.edu>
2020-02-03 21:51:42 -0600

    basic playable made

--- client/src/Game.js
+++ client/src/Game.js
@@ -1,30 +1,123 @@
 import Phaser from "phaser";
 import React from "react";
 
+let player, cursors;
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 320,
+  height: 640,
   scene: {
     preload: preload,
     create: create,
     update: update
   }
 };
+const objectMap = [];
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image('tiles', 'assets/gridtiles.png');
-  this.load.image('tiles', 'assets/gridtiles.png');
-  this.load.tilemapTiledJSON('map', 'assets/tutorial.json');
+  this.load.image("tilesBackground", "assets/gridtiles.png");
+  this.load.image("tiles", "assets/items.png");
+  this.load.tilemapTiledJSON("map", "assets/tutorial.json");
 }
 
 function create() {
-  const map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
-  const tileset = map.addTilesetImage('tiles');
-  const layer = map.createDynamicLayer('Level1', tileset);
+  const map = this.make.tilemap({ key: "map", tileWidth: 32, tileHeight: 32 });
+  const tileset = map.addTilesetImage("tiles");
+  const tileset2 = map.addTilesetImage("tilesBackground");
+  const backgroundLayer = map.createDynamicLayer("backgroundLayer", tileset2);
+  const blockedLayer = map.createDynamicLayer("blockedLayer", tileset2);
+  blockedLayer.putTileAt(290, 2, 2);
 
+  // set collision
+  // blockedLayer.setCollisionByProperty({ collides: true });
+  // console.log(blockedLayer.getTileAt(3, 7));
+  // blockedLayer.setCollision([290, 276]);
+  // console.log(blockedLayer.layer.data);
+
+  player = this.add.rectangle(32 + 16, 32 + 16, 26, 26, 0xffff00);
+  cursors = this.input.keyboard.createCursorKeys();
+
+  let currentTween;
+  const tweenDuration = 100;
+  const blockIndexCollidable = [290, 276];
+  function hasObstacle(plrX, plrY) {
+    const tile = blockedLayer.getTileAtWorldXY(plrX, plrY);
+    return tile && blockIndexCollidable.includes(tile.index);
+  }
+  cursors.up.on(
+    "down",
+    function() {
+      if (
+        (currentTween && currentTween.isPlaying()) ||
+        hasObstacle(player.x, player.y - 32)
+      ) {
+        return;
+      }
+
+      currentTween = this.tweens.add({
+        targets: player,
+        duration: tweenDuration,
+        y: player.y - 32
+      });
+    },
+    this
+  );
+  cursors.down.on(
+    "down",
+    function() {
+      if (
+        (currentTween && currentTween.isPlaying()) ||
+        hasObstacle(player.x, player.y + 32)
+      ) {
+        return;
+      }
+
+      currentTween = this.tweens.add({
+        targets: player,
+        duration: tweenDuration,
+        y: player.y + 32
+      });
+    },
+    this
+  );
+  cursors.left.on(
+    "down",
+    function() {
+      if (
+        (currentTween && currentTween.isPlaying()) ||
+        hasObstacle(player.x - 32, player.y)
+      ) {
+        return;
+      }
+
+      currentTween = this.tweens.add({
+        targets: player,
+        duration: tweenDuration,
+        x: player.x - 32
+      });
+    },
+    this
+  );
+  cursors.right.on(
+    "down",
+    function() {
+      if (
+        (currentTween && currentTween.isPlaying()) ||
+        hasObstacle(player.x + 32, player.y)
+      ) {
+        return;
+      }
+
+      currentTween = this.tweens.add({
+        targets: player,
+        duration: tweenDuration,
+        x: player.x + 32
+      });
+    },
+    this
+  );
 }
 
 function update() {}
@@ -41,16 +134,16 @@ export default Game;
  * characters = {demolisioner, recon, scout}
  * currentLevel = 0 - maxLevel
  * preload(){
- * 
+ *
  * }
- * 
+ *
  * create(){
  *  loadUI() //load ui of the game
  *  loadLevel() //loads tile for level
  * }
- * 
+ *
  * update(){
- *  
+ *
  *  //keyboard events
  *  if(currentCharacter !== enum.null){
  *    if(w is pressed){
@@ -60,8 +153,8 @@ export default Game;
  *    if(space pressed) toggleCharacter();
  *  }
  * }
- * 
- * 
+ *
+ *
  * characters <- demolisher, recon, scout
  * character{
  *  position {x, y}
