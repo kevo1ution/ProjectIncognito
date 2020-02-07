b9c61203333970914fef0d3fdc33d2a1edd3d3a3
Kevin Tang <kevin26428@gmail.com>
2020-02-07 14:02:37 -0600

    adding example movement

--- client/src/characters/character.js
+++ client/src/characters/character.js
@@ -1,11 +1,21 @@
 class Character {
-  constructor() {}
+  constructor(plrBody, moveDuration, scene) {
+    this.body = plrBody;
+    this.moveDuration = moveDuration;
+    this.scene = scene
+  }
 
   // Abstract virtual functions for moving up, down, left, right
-  moveUp() {}
-  moveDown() {}
-  moveLeft() {}
-  moveRight() {}
+  async moveUp(map) {
+    const targetPos = {x: this.body.x, y: this.body.y - 32};
+
+    if(map.hasObstacle(targetPos)){
+      
+    }
+  }
+  async moveDown(map) {}
+  async moveLeft(map) {}
+  async moveRight(map) {}
 }
 
 export default Character;
