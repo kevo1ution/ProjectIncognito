26d98ea404403110557db38160c15ba76bf81231
Kevin T <kdt0116@tamu.edu>
2020-03-02 22:14:26 -0600

    added lighting up tiles

--- client/src/maps/map.js
+++ client/src/maps/map.js
@@ -1,45 +1,108 @@
+import config from "../config/config";
+
 class Map {
   constructor(mapKey, scene) {
     this.map = scene.make.tilemap({
       key: mapKey,
-      tileWidth: 32,
-      tileHeight: 32
+      tileWidth: config.GAME.tileSize.x,
+      tileHeight: config.GAME.tileSize.y
     });
 
     this.tileset = this.map.addTilesetImage("tilesBackground");
-    this.blockIndexCollidable = [290, 276];
-    this.backgroundLayer = this.map.createDynamicLayer(
-      "backgroundLayer",
-      this.tileset
-    );
-    this.blockedLayer = this.map.createDynamicLayer(
-      "blockedLayer",
-      this.tileset
-    );
-    this.guardLayer = this.map.createDynamicLayer("guardLayer", this.tileset);
-    this.lightLayer = this.map.createDynamicLayer("lightLayer", this.tileset);
+    this.layers = {
+      background: this.map.createDynamicLayer("backgroundLayer", this.tileset),
+      blocked: this.map.createDynamicLayer("blockedLayer", this.tileset),
+      guard: this.map.createDynamicLayer("guardLayer", this.tileset),
+      light: this.map.createDynamicLayer("lightLayer", this.tileset)
+    };
 
-    this.guardMap = {}
+    this.guardMap = {};
+    this.layers.light.alpha = 0.5;
 
-    this.lightLayer.alpha = 0.2
+    this.setupLightLayer();
+
+    this.lightUp.bind(this);
   }
 
-  isGuardedTile(targetPos) {
-    return this.lightLayer.getTileAtWorldXY(targetPos.x, targetPos.y)
+  lightUp(pos, dir, num, tileIndex) {
+    const finalPos = { ...pos };
+    switch (dir) {
+      case 0: // right
+        pos.x++;
+        finalPos.x += num;
+        break;
+      case Math.PI / 2: // down
+        pos.y++;
+        finalPos.y += num;
+        break;
+      case Math.PI: // left
+        pos.x -= num;
+        finalPos.x--;
+        break;
+      case (Math.Pi * 3) / 2: // up
+        pos.y -= num;
+        finalPos.y--;
+        break;
+      default:
+        throw Error("Invalid direction!");
+    }
+
+    for (let x = pos.x; x <= finalPos.x; x++) {
+      for (let y = pos.y; y <= finalPos.y; y++) {
+        if (this.layers.light.getTileAt(x, y) === null) {
+          this.layers.light.putTileAt(tileIndex, x, y);
+        }
+      }
+    }
   }
 
-  rotateGuard(){
-    
+  setupLightLayer() {
+    //clear the previous lights
+    this.layers.light.replaceByIndex(327, -1);
+
+    const guardTiles = this.layers.guard.filterTiles(tile => {
+      return tile.index === config.GAME.tileIndex.guard;
+    });
+
+    const towerTiles = this.layers.guard.filterTiles(tile => {
+      return tile.index === config.GAME.tileIndex.tower;
+    });
+
+    guardTiles.forEach(tile => {
+      this.lightUp(
+        { x: tile.x, y: tile.y },
+        tile.rotation,
+        config.GAME.obstacle.guardSight,
+        config.GAME.tileIndex.light
+      );
+    });
+
+    towerTiles.forEach(tile => {
+      this.lightUp(
+        { x: tile.x, y: tile.y },
+        tile.rotation,
+        config.GAME.obstacle.towerSight,
+        config.GAME.tileIndex.light
+      );
+    });
   }
 
-  getBlockingTile(targetPos) {
+  isGuardedTile(pos) {
+    //iterate through the guards, and check if tile is within the range
+  }
+
+  rotateGuard(pos) {
+    //delete all the tiles from the light layer and remake them
+  }
+
+  getBlockingTile(pos) {
     return (
-      this.blockedLayer.getTileAtWorldXY(targetPos.x, targetPos.y) ||
-      this.guardLayer.getTileAtWorldXY(targetPos.x, targetPos.y)
+      this.layers.blocked.getTileAtWorldXY(pos.x, pos.y) ||
+      this.layers.guard.getTileAtWorldXY(pos.x, pos.y)
     );
   }
 
-  isMoveable(tile) {
+  isMoveable(pos, direction) {
     return false;
   }
 }
