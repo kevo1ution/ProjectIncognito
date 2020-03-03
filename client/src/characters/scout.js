38d88ae423f86e8ac3f55c2b77ed9a8353e474f9
Kevin Tang <kevin26428@gmail.com>
2020-03-03 16:22:42 -0600

    added rotating ability for scout

--- client/src/characters/scout.js
+++ client/src/characters/scout.js
@@ -1,7 +1,19 @@
 import Character from "./character";
+import config from "../config/config";
 
 class Scout extends Character {
+  ability(map) {
+    const guardsPositions = [
+      { x: this.body.x + config.GAME.tileSize.x, y: this.body.y },
+      { x: this.body.x - config.GAME.tileSize.x, y: this.body.y },
+      { x: this.body.x, y: this.body.y + config.GAME.tileSize.y },
+      { x: this.body.x, y: this.body.y - config.GAME.tileSize.y }
+    ];
 
+    guardsPositions.forEach(pos => {
+      map.rotateGuardOrTower(pos);
+    });
+  }
 }
 
-export default Scout
+export default Scout;
