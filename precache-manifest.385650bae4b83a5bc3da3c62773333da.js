60324cb504d8f8e7a21ee9e90e8ba9e6a2c0f6db
Kevin T <kdt0116@tamu.edu>
2020-04-01 14:55:35 -0500

    Updates

---
+++ precache-manifest.385650bae4b83a5bc3da3c62773333da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec03e16ef71360471e80ed633bf7bbb",
+    "url": "/ProjectIncognito/index.html"
+  },
+  {
+    "revision": "bb67cc6b7f8107778777",
+    "url": "/ProjectIncognito/static/js/2.f8ab54da.chunk.js"
+  },
+  {
+    "revision": "55d0b47d8b013b455d198284c65e90ca",
+    "url": "/ProjectIncognito/static/js/2.f8ab54da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc7b6282e55bcc5796b4",
+    "url": "/ProjectIncognito/static/js/main.8ed74a30.chunk.js"
+  },
+  {
+    "revision": "73e4600420ec5f86739a",
+    "url": "/ProjectIncognito/static/js/runtime-main.72de92b7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projectincognito/precache-manifest.3762bfe967960315435c977805a1262e.js"
+  "/ProjectIncognito/precache-manifest.385650bae4b83a5bc3da3c62773333da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projectincognito/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ProjectIncognito/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
